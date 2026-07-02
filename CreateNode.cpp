e0a1d3573b9ebf0eb94d90cd8a42e1f7335f69e1
Binoy Sarker <binoysarker2003@gmail.com>
2026-07-02 13:03:20 +0600

    Add files via upload

---
+++ CreateNode.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    struct node *next;
+};
+int main()
+{
+    node *first=(node*) malloc(sizeof(node));
+    first->data=9;
+    first->next=NULL;
+
+
+
+}
